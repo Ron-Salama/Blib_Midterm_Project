8408203d49b0fbb51b7ea4db3854bbc20719819d
KoganTheDev <Yuvalkogan123@gmail.com>
2025-01-09 16:23:08 +0200

    Revert "Merge branch 'Requests'"
    This reverts commit 1720832f30572a76ce04873c643fc5fa8512ee89, reversing
    changes made to d0f333a49def912e9c5c687ebb5cfa19904cec96.

---
+++ src/Client/src/logic/SubscriberRequest.java
@@ -0,0 +1,14 @@
+package logic;
+
+import java.util.ArrayList;
+import logic.SubscriberRequestType;
+
+public class SubscriberRequest {
+	private ArrayList<String> requestList = new ArrayList<>();
+	
+	public ArrayList<String> getRequestList() {
+		return this.requestList;
+	}
+	
+	
+}
