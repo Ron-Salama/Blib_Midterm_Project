dc3ff337cf518ec42d740be7911ec12e7bb24f82
Almog Raz <almograz1@gmail.com>
2025-01-14 21:19:08 +0200

    my books update

--- src/Client/src/logic/ClientTimeDiffController.java
+++ src/Client/src/logic/ClientTimeDiffController.java
@@ -9,9 +9,32 @@ public class ClientTimeDiffController {
 	public Boolean hasEnoughTimeBeforeDeadline(LocalDateTime today, LocalDateTime deadLine, int timeNeededBeforeDeadline) {
 		LocalDateTime sevenDaysFromDeadline = deadLine.minusDays(timeNeededBeforeDeadline);
 		if (sevenDaysFromDeadline.isAfter(today)) { // Meaning there isn't enough time to act before the deadline.
-			return false;
+			return false; // display message to client screen
 		}
 		
+<<<<<<< Updated upstream
 		return true; // Meaning there's enough time to act before the deadline.
 		}
+<<<<<<< Updated upstream
+=======
+=======
+		return true; // Meaning there's enough time to act before the deadline. // perform extend (Almog)
+	}
+>>>>>>> Stashed changes
+	
+	public String calculateReturnDate(LocalDateTime today, int amountOfDays) {
+		LocalDateTime returnDate = today.plusDays(amountOfDays);
+		return returnDate.format(dateFormatter).toString();
+	}
+	
+	public String extendReturnDate(LocalDateTime returnDate, int amountOfDays) {
+		LocalDateTime returnDateAfterExtension = returnDate.plusDays(amountOfDays);
+		return returnDateAfterExtension.format(dateFormatter).toString();
+	}
+	
+	public String timeNow() {
+		LocalDateTime now = LocalDateTime.now();
+		return now.format(dateFormatter);
+	}
+>>>>>>> Stashed changes
 }
