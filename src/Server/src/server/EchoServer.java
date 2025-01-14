81492a444680337a0d75a9ec456e3c9d7766d8ef
KoganTheDev <Yuvalkogan123@gmail.com>
2025-01-14 22:31:06 +0200

    update the name of the clock controller on the server side

--- src/Server/src/server/EchoServer.java
+++ src/Server/src/server/EchoServer.java
@@ -10,7 +10,6 @@ import java.sql.SQLException;
 import java.util.List;
 
 import common.ConnectToDb;
-import logic.ClockController;
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
 
